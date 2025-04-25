d27d982946438bac8782460232dfa27dddb2b7a8
TymKh <tkhrushchov@gmail.com>
2025-04-25 12:54:23 +0200

    Fetch raw config values from secrets (#56)
    ## 📝 Summary
    
    Simplify configuration process. Fetch json config as is from aws secrets
    manager.
    
    ## ⛱ Motivation and Context
    
    Previous way was forcing us to struggle with escaping/unescaping a lot
    
    ## 📚 References
    
    ---
    
    ## ✅ I have run these commands
    
    * [x] `make lint`
    * [x] `make test`
    * [x] `go mod tidy`

--- domain/inmemory_secret.go
+++ domain/inmemory_secret.go
@@ -1,26 +1,29 @@
 package domain
 
-import "sync"
+import (
+	"encoding/json"
+	"sync"
+)
 
 type InmemorySecretService struct {
 	mu *sync.RWMutex
-	st map[string]map[string]string
+	st map[string]json.RawMessage
 }
 
 func NewMockSecretService() *InmemorySecretService {
 	return &InmemorySecretService{
-		st: make(map[string]map[string]string),
+		st: make(map[string]json.RawMessage),
 		mu: &sync.RWMutex{},
 	}
 }
 
-func (mss *InmemorySecretService) GetSecretValues(builderName string) (map[string]string, error) {
+func (mss *InmemorySecretService) GetSecretValues(builderName string) (json.RawMessage, error) {
 	mss.mu.RLock()
 	defer mss.mu.RUnlock()
 	return mss.st[builderName], nil
 }
 
-func (mss *InmemorySecretService) SetSecretValues(builderName string, values map[string]string) error {
+func (mss *InmemorySecretService) SetSecretValues(builderName string, values json.RawMessage) error {
 	mss.mu.Lock()
 	defer mss.mu.Unlock()
 	mss.st[builderName] = values
